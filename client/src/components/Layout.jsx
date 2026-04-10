876c3e7be643724b64513727e509c4efbb0cdaff
Thejus020 <007thejuthejus@gmail.com>
2026-04-11 03:50:08 +0530

    style: use GitHub Octicon three-bars for mobile hamburger menu

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -18,8 +18,8 @@ export default function Layout({ children }) {
             onClick={() => setMobileOpen(true)}
             className="p-2 text-gray-400 hover:text-white rounded-lg hover:bg-gray-800 transition"
           >
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            <svg aria-hidden="true" focusable="false" className="w-5 h-5 fill-current" viewBox="0 0 16 16" display="inline-block" overflow="visible" style={{ verticalAlign: 'text-bottom' }}>
+              <path d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z"></path>
             </svg>
           </button>
         </header>
